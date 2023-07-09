db80ef1b7ca855a9a02435bbe2ad73e2e9b3ba62
mayonex <99794276+mayonex@users.noreply.github.com>
2023-07-09 12:04:30 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,17 +1,9 @@
-# 1158
 import sys
 N, K = map(int, sys.stdin.readline().split(" "))
-circle = []
+circle = [i for i in range(1, N + 1)]
 answer = []
 index = 0
-for i in range(1, N + 1):
-    circle.append(i)
 while(len(circle) > 0):
     index = (index + K - 1) % len(circle)
     answer.append(str(circle.pop(index)))
-answer_str = ", ".join(answer)
-pre = list(answer_str)
-pre.insert(0, '<')
-pre.insert(len(pre), '>')
-print("".join(pre))
- 
+print("<", ", ".join(answer), ">", sep="")
