6d62af3ac8e6e6ce31c49de62038e68adad6faa3
mayonex <99794276+mayonex@users.noreply.github.com>
2023-08-04 18:18:16 +0900

    [level 2] Title: 주식가격, Time: 10.64 ms, Memory: 43.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.js
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.js
@@ -1,18 +1,37 @@
-function solution(prices){
-    const stack = [];
-    const counts = [];
-    for(let i = 0; i < prices.length; i++){
-        const price = prices[i];
-        const sec = i;
-        while(stack.length > 0 && stack[stack.length - 1][0] > price){
-            const [prevPrice, prevSec] = stack.pop()
-            counts[prevSec] = sec - prevSec;
-        }
-        stack.push([price, sec]);
+// 코테 문제 1
+
+// function isVaild(s) {
+//   const stack = [];
+//   for (i of s) {
+//     if (i == "(") {
+//       stack.push(")");
+//     } else if (i == "{") {
+//       stack.push("}");
+//     } else if (i == "[") {
+//       stack.push("]");
+//     } else if (i != stack.pop()) {
+//       return false;
+//     }
+//   }
+//   if (stack.length) {
+//     return false;
+//   }
+//   return true;
+// }
+// 코테 문제 2
+function solution(temperatures) {
+  const answer = new Array(temperatures.length).fill(0);
+  const stack = [];
+  let prevDay, prevTemp;
+  let curDay, curTemp;
+  for (let i = 0; i < temperatures.length; i++) {
+    curDay = i;
+    curTemp = temperatures[i];
+    while (stack.length && curTemp < stack[stack.length - 1][1]) {
+      [prevDay, prevTemp] = stack.pop();
+      answer[prevDay] = curDay - prevDay;
     }
-    while(stack.length > 0){
-        const [prevPrice, prevSec] = stack.pop();
-        counts[prevSec] = prices.length - 1 - prevSec;
-    }
-    return counts;
+    stack.push([curDay, curTemp]);
+  }
+  return answer;
 }
