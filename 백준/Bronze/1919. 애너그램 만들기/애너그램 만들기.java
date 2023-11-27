cabc44507f1a8f986285264b4a93e82af50020fc
mayonex <99794276+mayonex@users.noreply.github.com>
2023-11-27 19:35:07 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 148 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+// 1
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] A, B;
+        A = br.readLine().toCharArray();
+        B = br.readLine().toCharArray();
+
+        ArrayList<Character> listA = new ArrayList<>();
+        for(char c : A){
+            listA.add(c);
+        }
+
+        ArrayList<Character> listB = new ArrayList<>();
+        for(char c : B){
+            listB.add(c);
+        }
+
+
+        ArrayList<Character> curListA = new ArrayList<>();
+        ArrayList<Character> curListB = new ArrayList<>();
+
+
+        for(char c : A){
+            if(listB.contains(c)){
+                curListA.add(c);
+            }
+        }
+
+        for(char c : B){
+            if(listA.contains(c)){
+                curListB.add(c);
+            }
+        }
+        int deleteCount = 0;
+        deleteCount = deleteCount +  (listA.size() - curListA.size())  + (listB.size() - curListB.size());
+
+        Set<Character> uniqueChars = new HashSet<>();
+
+        if (!curListA.isEmpty()) {
+            uniqueChars.addAll(curListA);
+        } else {
+            uniqueChars.addAll(curListB);
+        }
+
+        ArrayList<Character> uniqueList = new ArrayList<>(uniqueChars);
+
+
+        for(char c : uniqueList){
+            int countA = Collections.frequency(curListA, c);
+            int countB = Collections.frequency(curListB, c);
+            deleteCount = deleteCount +  Math.abs(countA - countB);
+        }
+
+        System.out.print(deleteCount);
+    }
+}
